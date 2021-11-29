3830c5b2554d1d8b2af7b7897fa48a4dce6ce57d
Ben <you@example.com>
2021-11-29 08:37:22 +0000

    Upload chapter2

--- ExercisesChapter2.py
+++ ExercisesChapter2.py
@@ -209,7 +209,7 @@
 # print(entry_frequency)
 #########################################################################################
 
-# Exercise 41 - Note to Frequency
+# Exercise 43 - Faces on Money
 # It is common for images of a country’s previous leaders, or other individuals of historical significance, to appear on its money. The individuals that appear on banknotes
 # in the United States are listed in Table 2.1. Write a program that begins by reading the denomination of a banknote from the
 # user. Then your program should display the name of the individual that appears on the banknote of the entered amount. An appropriate error message should be displayed if no such note exists
