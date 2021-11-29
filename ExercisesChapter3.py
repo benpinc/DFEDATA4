9a133c473fd55b0c8fe540b67b8b53e18b2612a0
Ben <you@example.com>
2021-11-29 11:36:02 +0000

    Added exercise 62

--- ExercisesChapter3.py
+++ ExercisesChapter3.py
@@ -23,3 +23,22 @@
 # avg = numbers / count 
 # print("The average is ", avg)
 #####################################################################
+# Exercise 62: Discount Table
+# (18 Lines)
+# A particular retailer is having a 60 percent off sale on a variety of discontinued
+# products. The retailer would like to help its customers determine the reduced price
+# of the merchandise by having a printed discount table on the shelf that shows the
+# original prices and the prices after the discount has been applied. Write a program that
+# uses a loop to generate this table, showing the original price, the discount amount,
+# and the new price for purchases of $4.95, $9.95, $14.95, $19.95 and $24.95. Ensure
+# that the discount amounts and the new prices are rounded to 2 decimal places when
+# they are displayed.
+#
+# import math
+# original_price = [4.95, 9.95, 14.95, 19.95, 24.95]
+# discount = 0.6
+# for i in original_price:
+#     discount_amount = float(i * discount)
+#     print("${:.2f}".format(i), "{:.0%}".format(discount), "${:.2f}".format(discount_amount))
+# #sale_price = float(original_price - discount_amount)
+
