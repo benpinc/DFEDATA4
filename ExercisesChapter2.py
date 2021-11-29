e465095ac48d2f84c6c4584a728e1bfb2bf8e8d5
Ben <you@example.com>
2021-11-29 08:29:04 +0000

    Added Exercise 41

--- ExercisesChapter2.py
+++ ExercisesChapter2.py
@@ -208,3 +208,37 @@
 # entry_frequency = base_result/2**(4-entry_octave)
 # print(entry_frequency)
 #########################################################################################
+
+# Exercise 41 - Note to Frequency
+# It is common for images of a country’s previous leaders, or other individuals of historical significance, to appear on its money. The individuals that appear on banknotes
+# in the United States are listed in Table 2.1. Write a program that begins by reading the denomination of a banknote from the
+# user. Then your program should display the name of the individual that appears on the banknote of the entered amount. An appropriate error message should be displayed if no such note exists
+# Table 2.1 Individuals that appear on Banknotes 
+
+# Individual | Amount
+# George Washington | $1
+# Thomas Jefferson | $2
+# Abraham Lincoln | $5
+# Alexander Hamilton | $10
+# Andrew Jackson | $20
+# Ulysses S. Grant | $50
+# Benjamin Franklin | $100
+#
+# entry = int(input("Enter the banknote: "))
+# if entry == 1:
+#     print("George Washington")
+# elif entry == 2:
+#     print("Thomas Jefferson")
+# elif entry == 5:
+#     print("Abraham Lincoln")
+# elif entry == 10:
+#     print("Alexander Hamilton")
+# elif entry == 20:
+#     print("Andrew Jackson")
+# elif entry == 50:
+#     print("Ulysses S. Grant")
+# elif entry == 100:
+#     print("Benjamin Franklin")
+# else:
+#     print("No such banknote")
+#########################################################################################
