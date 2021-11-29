6ea166ada06fc1cdb4811773bf8fed7f90ec0bb4
Ben <you@example.com>
2021-11-29 09:51:23 +0000

    Updated with exercise

---
+++ ExercisesChapter3.py
@@ -0,0 +1,25 @@
+# The Python Workbook by Ben Stephenson
+# Chapter 3 - Loop Exercises
+#################################################################
+# Exercise 61
+# 
+# In this exercise you will create a program that computes the average of a collection
+# of values entered by the user. The user will enter 0 as a sentinel value to indicate
+# that no further values will be provided. Your program should display an appropriate
+# error message if the first value entered by the user is 0.
+#
+# numbers = 0
+# count = 0
+
+# zero_exit = False
+# while not zero_exit:
+# 	entry = int(input("Enter a value: "))
+# 	if entry != 0:
+# 		numbers = numbers + entry
+# 		count = count + 1
+# 	else:
+# 		zero_exit = True 
+		
+# avg = numbers / count 
+# print("The average is ", avg)
+#####################################################################
